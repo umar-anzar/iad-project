42b1a44ea30a8f4f710c08d1207258decd4e2636
Muhammad Umar Anzar <63141196+umar-anzar@users.noreply.github.com>
2023-06-20 04:41:42 +0500

    UP 3 page done

---
+++ website/public/javascripts/work_content.js
@@ -0,0 +1,32 @@
+const company = "Assisted in the structural design and analysis of the building. <br> Conducted site visits to ensure construction compliance with design specifications";
+const education = "Bachelors <br> Xyz Science <br> The Abc University <br> 2016-2020 <br> CGPA: 3.57";
+const skills = "Structural Analysis <br> Structural Design <br> Building Codes and Regulations <br> AutoCAD <br> Finite Element Analysis <br> Building Information Modeling";
+
+const title = {
+    company:"Company Name",
+    education:"Education",
+    skills:"Skills"
+};
+
+const content = {
+    company,
+    education,
+    skills
+};
+
+document.addEventListener('DOMContentLoaded', function() {
+    var work_box = document.querySelectorAll('.work_box');
+    work_box.forEach(function(box) {
+
+        for (let key in content) {
+            if (box.id === key) {
+                box.addEventListener('mouseenter', () => (box.innerHTML = content[key]));
+                box.addEventListener('mouseleave', () => (box.innerHTML = title[key]));
+            }
+        }
+    });
+});
+
+
+
+  
